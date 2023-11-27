21dd1b61288b5487ec4c3c1c66be7761b2bcc54a
Vildarodinson <117757981+Vildarodinson@users.noreply.github.com>
2023-11-26 21:53:14 -0800

    feat: added client side for register

---
+++ public/frontend/register.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const registerForm = document.getElementById("registerForm");
+
+  registerForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      const response = await fetch("/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        console.log("Registration successful!");
+
+        window.location.href = "login.html";
+      } else {
+        const errorData = await response.json();
+        console.error(`Registration failed: ${errorData.error}`);
+      }
+    } catch (error) {
+      console.error("Error during registration:", error);
+      alert("Internal server error");
+    }
+  });
+});
